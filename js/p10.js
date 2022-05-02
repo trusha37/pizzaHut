dcfc326a1e8e890551624d98c16207b13a301d7e
trusha <trusha.vithlani1@gmail.com>
2022-05-02 15:54:15 +0530

    Hosting my Site

---
+++ js/p10.js
@@ -0,0 +1,48 @@
+var b1 = document.getElementById("b1") 
+var b2 = document.getElementById("b2") 
+var b3 = document.getElementById("b3") 
+var b4 = document.getElementById("b4") 
+var b5 = document.getElementById("b5") 
+
+window.onload = Btn1;
+
+function Btn1()
+{
+    b1.style.display = "block";
+    b2.style.display = "none";
+    b3.style.display = "none";
+    b4.style.display = "none";
+    b5.style.display = "none";
+}
+function Btn2()
+{
+    b1.style.display = "none";
+    b2.style.display = "block";
+    b3.style.display = "none";
+    b4.style.display = "none";
+    b5.style.display = "none";
+}
+function Btn3()
+{
+    b1.style.display = "none";
+    b2.style.display = "none";
+    b3.style.display = "block";
+    b4.style.display = "none";
+    b5.style.display = "none";
+}
+function Btn4()
+{
+    b1.style.display = "none";
+    b2.style.display = "none";
+    b3.style.display = "none";
+    b4.style.display = "block";
+    b5.style.display = "none";
+}
+function Btn5()
+{
+    b1.style.display = "none";
+    b2.style.display = "none";
+    b3.style.display = "none";
+    b4.style.display = "none";
+    b5.style.display = "block";
+}
